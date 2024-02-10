bf0870b181d9dad47620d20c88658d668db6f6af
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-02-10 11:13:37 +0600

    lab2

---
+++ Lab 2/lab2.py
@@ -0,0 +1,18 @@
+############################# Task 1 #################################
+######################################################################
+
+input_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab 2/input1.txt'
+output_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab 2/output1.txt'
+
+#with open(input_path, 'r') as in_file:
+ #   content = in_file.read()
+
+#with open(output_path, 'w') as out_file:
+ #   out_file.write(content)
+ 
+inp2 = open(input_path, 'r')
+
+#print(content)
+firstline = inp2.readline()
+
+print(firstline)
