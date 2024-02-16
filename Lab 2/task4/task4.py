f2d8644c1318178b1d002583a092c62ab412b4d9
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-02-16 16:03:56 +0600

    lab2

--- Lab 2/task4/task4.py
+++ Lab 2/task4/task4.py
@@ -18,30 +18,40 @@ N, M = map(int, inp.readline().split())
 intervals = [tuple(map(int, inp.readline().strip().split())) for i in range(N)]  #Reading all intervals and converting to tuple
 
 
+
+#Function to minimize code
+def max_task(n, m, intervals):
+    # Initialize schedule for each person
+    schedule = [0] * M
+
+    # Count of tasks completed
+    completed_tasks = 0
+
+    # Assign tasks greedily
+    for activity in intervals:
+        start, end = activity
+        
+        # Find the earliest available person
+        for i in range(M):
+            if schedule[i] <= start:
+                schedule[i] = end
+                completed_tasks += 1
+                break
+    return completed_tasks
+
+
 # Sort intervals based on their end times
-intervals.sort(key=lambda x: x[1])
+sorted_on_endtime = sorted(intervals, key=lambda x: x[1])
 
-# Initialize schedule for each person
-schedule = [0] * M
+# Sort intervals based on their start times
+sorted_on_starttime = sorted(intervals, key=lambda x: x[0])
 
-# Count of tasks completed
-completed_tasks = 0
-
-print(intervals)
-# Assign tasks greedily
-for activity in intervals:
-    start, end = activity
-    
-    # Find the earliest available person
-    for i in range(M):
-        if schedule[i] <= start:
-            schedule[i] = end
-            completed_tasks += 1
-            break
-
-
-# Output
-out.write(str(completed_tasks))
-print(completed_tasks)
+#choosing biggest combination of tasks
+if(max_task(N, M, sorted_on_endtime) >= max_task(N, M, sorted_on_starttime)):
+    out.write(str(max_task(N, M, sorted_on_endtime)))
+    print(max_task(N, M, sorted_on_endtime))
+else:
+    out.write(str(max_task(N, M, sorted_on_starttime)))
+    print(max_task(N, M, sorted_on_starttime))
 
 out.close()         #Closing output file to save and work properly 
