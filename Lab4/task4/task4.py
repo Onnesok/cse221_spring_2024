e952cd807fe59e1488a572d3a678863126062bb0
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-03-18 10:30:09 +0600

    lab4

--- Lab4/task4/task4.py
+++ Lab4/task4/task4.py
@@ -4,7 +4,7 @@
 #Specify path
 input_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab4/task4/input1_1.txt'
 output_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab4/task4/output1_1.txt'
-#OPen file
+#Open file
 inp = open(input_path, 'r')
 out = open(output_path, 'w')
 #Read and split
@@ -17,21 +17,41 @@ arr = [[] for i in range (n + 1)]
 for i in range (m):
     u, v = inp.readline().strip().split(' ')
     arr[int(u)].append(int(v))
-    arr[int(v)].append(int(u))
-  
-#print(arr)
-def DFS(g, start):
-    chk = []                # For checking stack
-    stack = [start]         # stack
-    res = ''                  # Main result string
-    while stack:              # If stack not empty loop will work
-        node = stack.pop()      # pop first and check if already exist in chk
-        if node not in chk:
-            res += str(node) + ' '      # If dont then thats one vertex
-            chk.append(node)            # Insert to chk to keep track that we've used it
-            stack.extend(reversed(g[node]))     # we need reverse value, so reversed and extended to array
-    out.write(res)      
-    print(res)
-    
-DFS(arr, 1)
+
+print(arr)
+
+def DFS(g, start, visited, stack):
+    visited[start] = True
+    stack[start] = True
+    for neighbor in g[start]:
+        if not visited[neighbor]:
+            if DFS(g, neighbor, visited, stack):
+                return True
+        elif stack[neighbor]:
+            return True
+
+    stack[start] = False
+    return False
+
+def hasCycle(g):
+    visited = [False] * (n + 1)
+    stack = [False] * (n + 1)
+
+    for node in range(1, n + 1):
+        if not visited[node]:
+            if DFS(g, node, visited, stack):
+                return True
+    return False
+
+if hasCycle(arr):
+    out.write("YES")
+    print("YES")
+else:
+    out.write("NO")
+    print("NO")
+
 out.close()
+
+
+
+##### ki hoise bujhi nai
