10933d27c257a3debc4a27e3e0859124472bc6dc
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-04-18 13:36:09 +0600

    task2

--- Lab6/task2/task2.py
+++ Lab6/task2/task2.py
@@ -1,34 +1,24 @@
 ########################################### Task2  ##################################
 #####################################################################################
-import math
 import heapq as hq
 
-def dijkstra(dc, s):
-    dist = [math.inf]*len(dc)
+def dijkstra(g, s):
+    dist = {vertex: float('inf') for vertex in g}
     dist[s] = 0
-    q = []
-    hq.heappush(q, (dist[s], s))
-    prev = [None]*len(dc)
-    visited = [False]*len(dc)
-    cost = 0
+    q = [(0, s)]
     
     while q:
-        num, u = hq.heappop(q)
-        if visited[u] == True:
+        current_dist, u = hq.heappop(q)
+        if current_dist > dist[u]:
             continue
-        visited[u] = True
-        for v, cost in dc[u]:
-            alt = dist[u] + cost
+        for v, w in g[u]:
+            alt = current_dist + w
+            
             if alt < dist[v]:
                 dist[v] = alt
-                prev[v] = u
-                hq.heappush(q, (dist[v], v))
-                
-    dist.pop(0)
-    while math.inf in dist:
-        idx = dist.index(math.inf)
-        dist[idx] = -1
+                hq.heappush(q, (alt, v))        
     return dist
+
 
 #Specify path
 input_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab6/task2/input.txt'
@@ -36,31 +26,40 @@ output_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab6/task2/output.txt'
 #Open file
 inp = open(input_path, 'r')
 out = open(output_path, 'w')
-#Reading all input at once and slicing it
-inp = inp.read().split("\n")
-line1 = inp[0].split(" ")
+#Read and split
+n, m = map(int, inp.readline().split())
+graph = {i:[] for i in range(n+1)}
 
-nodes = int(line1[0])
-edges = int(line1[1])
-source = int(inp[-1])
+for i in range(m):
+    u, v, w = map(int, inp.readline().split())
+    graph[u].append((v, w))
 
-dc = {i:[] for i in range(nodes+1)}
+#starting of alice and bob    
+s, t = map(int, inp.readline().split())
 
-for i in range(1, edges+1, 1):
-    info = inp[i].split(" ")
-    node1 = int(info[0])
-    node2 = int(info[1])
-    cost = int(info[2])
-    arr = dc[node1]
-    arr.append((node2, cost))
+#For alice
+ds = dijkstra(graph, s)
+#For bob
+dt = dijkstra(graph, t)
+
+min_time = float('inf')
+meet = None
+for v in range(1, n+1):
+    if ds[v] != float('inf') and dt[v] != float('inf'):
+        t = max(ds[v], dt[v])
+        if t < min_time:
+            min_time = t
+            meet = v
+
+if meet == None or min_time == float('inf'):
+    out.write("Impossible")
+    print("Impossible")
+else:
+    out.write(f"Time {min_time}\nNode {meet}")
+    print(f"Time {min_time}\nNode {meet}")
     
-costs = dijkstra(dc, source)
-out.write(" ".join(map(str, costs)))
-print(" ".join(map(str, costs)))
-
-out.close()  #closing for safely saving
+out.close()
 
 #Explanation
-#Here input is taken from a specified file and read() is used to read files all at once and then sliced and got the n, m and source. Now created a dictonary and populated those with the values of nodes and costs.
-#Now dijkstra() algorithm is used to determine shortest path. In the dijkstra() function it takes dc and source. It returns list of shortest distance from the source to every nodes. Now print the list after slicing and joining
-#using .join() and we got our answer.
+#Here specified an input and output file and then opened it in the code and used readline() to read file line by line. In last line ds is for starting node of alice and dt is for starting node of bob. Now passing them in dijkstra algo function
+#and got a path list. Now initialize min_time and meet variable and check after the iteration if its same or not. If not then the min_time is Time and meet is Node of the output. Now write them in the file and print output. Done.
