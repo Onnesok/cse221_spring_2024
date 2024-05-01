96eeace49dec45d896465ef06ac322a95467e14e
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-05-01 14:20:15 +0600

    task 4

--- Lab7/task4/task4.py
+++ Lab7/task4/task4.py
@@ -1,9 +1,7 @@
-def min_coins_needed(N, X, denominations):
-    # Initialize an array to store the minimum number of coins required for each amount
+def min_coins(N, X, denominations):
     dp = [float('inf')] * (X + 1)
     dp[0] = 0  # Base case: 0 coins needed to make up amount 0
 
-    # Iterate through each coin denomination
     for coin in denominations:
         # Update the dp array for each possible amount
         for amount in range(coin, X + 1):
@@ -15,9 +13,25 @@ def min_coins_needed(N, X, denominations):
     else:
         return dp[X]
 
+
+
+#Specify input file path
+input_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab7/task4/input.txt'
+output_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab7/task4/output.txt'
+#Open file
+inp = open(input_path, 'r')
+out = open(output_path, 'w')
+
 # Input
-N, X = map(int, input().split())
-denominations = list(map(int, input().split()))
+N, X = map(int, inp.readline().split())
+denominations = list(map(int, inp.readline().split()))
 
 # Output
-print(min_coins_needed(N, X, denominations))
+print(min_coins(N, X, denominations))
+out.write(str(min_coins(N, X, denominations)))
+
+out.close()
+
+#Explanation
+# Taking input from specified file and getting N(number of different coin dimension) and X(target amount of money). In min_coins() sending N, x and dimensions. Initializing an array to store the minimum number of coins required for each amount.
+# Iterating through each coin denomination and updating dp array for each possible amount. Lastly checking if its possible to make target amount and returning output and printing.
