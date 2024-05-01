15b2f1b375bad7cd78a545eb9808b736ddfbfc79
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2024-05-01 13:35:04 +0600

    task2

--- Lab7/task2/task2.py
+++ Lab7/task2/task2.py
@@ -26,9 +26,25 @@ def kruskal(N, edges):
             mst_cost += w
     return mst_cost
 
-# Input
-N, M = map(int, input().split())
-edges = [list(map(int, input().split())) for _ in range(M)]
+
+
+
+#Specify input file path
+input_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab7/task2/input.txt'
+output_path = 'C:/Users/ASUS/Desktop/cse221_spring_2024/Lab7/task2/output.txt'
+#Open file
+inp = open(input_path, 'r')
+out = open(output_path, 'w')
+
+# Reading input
+N, M = map(int, inp.readline().split())
+
+edges = [list(map(int, inp.readline().split())) for _ in range(M)]
 
 # Output
 print(kruskal(N, edges))
+out.write(str(kruskal(N, edges)))
+
+#Explanation
+#Here took input from a specified path and split and sent to kruskal() function. It takes N which is number of cities and edges. Inside function initialized parents and rank and sorted edges based on their maintainance costs in ascending order.
+#Then if u and v is not in the same set, they are united using union() function. This is continuing until all the cities are connected in a single MST. Lastly, mst_cost is returned and print and also write in output file. out.close() for safely saving.
